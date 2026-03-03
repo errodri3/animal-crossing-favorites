4460badfee797db2fd336d2b3e7cdc5c1e2576d9
Evelyn Rodriguez <errodriguez0703@gmail.com>
2026-03-03 10:28:18 -0800

    add villager data and card component

--- Documents/WEB102/animal-crossing-favorites/src/App.jsx
+++ Documents/WEB102/animal-crossing-favorites/src/App.jsx
@@ -1,7 +1,30 @@
+import villagers from './data';
+import VillagerCard from './VillagerCard';
+import './App.css';
+
 function App() {
   return (
-    <div>
-      <h1>Hello!</h1>
+    <div className="app">
+      <header className="header">
+        <h1>🌿 Animal Crossing Favorites 🌿</h1>
+        <p>Meet the most beloved villagers from the island!</p>
+      </header>
+
+      <div className="card-grid">
+        {villagers.map((villager) => (
+          <VillagerCard
+            key={villager.id}
+            name={villager.name}
+            species={villager.species}
+            personality={villager.personality}
+            catchphrase={villager.catchphrase}
+            favoriteColor={villager.favoriteColor}
+            birthday={villager.birthday}
+            image={villager.image}
+            wiki={villager.wiki}
+          />
+        ))}
+      </div>
     </div>
   );
 }
