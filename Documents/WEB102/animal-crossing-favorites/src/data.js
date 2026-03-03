fc5ecbbe1cbf09a8e84803af15296febce17f477
Evelyn Rodriguez <errodriguez0703@gmail.com>
2026-03-03 10:25:33 -0800

    Creating Villager data file

---
+++ Documents/WEB102/animal-crossing-favorites/src/data.js
@@ -0,0 +1,114 @@
+const villagers = [
+  {
+    id: 1,
+    name: "Marshal",
+    species: "Squirrel",
+    personality: "Smug",
+    catchphrase: "sulky",
+    favoriteColor: "Gray",
+    birthday: "September 29",
+    image: "https://dodo.ac/np/images/e/ee/Marshal_NH.png",
+    wiki: "https://animalcrossing.fandom.com/wiki/Marshal"
+  },
+  {
+    id: 2,
+    name: "Raymond",
+    species: "Cat",
+    personality: "Smug",
+    catchphrase: "crisp",
+    favoriteColor: "Gray",
+    birthday: "October 1",
+    image: "https://dodo.ac/np/images/c/c4/Raymond_NH.png",
+    wiki: "https://animalcrossing.fandom.com/wiki/Raymond"
+  },
+  {
+    id: 3,
+    name: "Isabelle",
+    species: "Dog",
+    personality: "Normal",
+    catchphrase: "what what",
+    favoriteColor: "Yellow",
+    birthday: "December 20",
+    image: "https://dodo.ac/np/images/9/98/Isabelle_NH.png",
+    wiki: "https://animalcrossing.fandom.com/wiki/Isabelle"
+  },
+  {
+    id: 4,
+    name: "Tom Nook",
+    species: "Raccoon",
+    personality: "Cranky",
+    catchphrase: "yes yes",
+    favoriteColor: "Brown",
+    birthday: "May 30",
+    image: "https://dodo.ac/np/images/9/90/Tom_Nook_NH.png",
+    wiki: "https://animalcrossing.fandom.com/wiki/Tom_Nook"
+  },
+  {
+    id: 5,
+    name: "Ankha",
+    species: "Cat",
+    personality: "Snooty",
+    catchphrase: "me meow",
+    favoriteColor: "Yellow",
+    birthday: "September 22",
+    image: "https://dodo.ac/np/images/8/8c/Ankha_NH.png",
+    wiki: "https://animalcrossing.fandom.com/wiki/Ankha"
+  },
+  {
+    id: 6,
+    name: "Zucker",
+    species: "Octopus",
+    personality: "Lazy",
+    catchphrase: "bloop",
+    favoriteColor: "Orange",
+    birthday: "March 8",
+    image: "https://dodo.ac/np/images/c/cd/Zucker_NH.png",
+    wiki: "https://animalcrossing.fandom.com/wiki/Zucker"
+  },
+  {
+    id: 7,
+    name: "Judy",
+    species: "Cub",
+    personality: "Snooty",
+    catchphrase: "myohmy",
+    favoriteColor: "Pink",
+    birthday: "March 10",
+    image: "https://dodo.ac/np/images/3/3b/Judy_NH.png",
+    wiki: "https://animalcrossing.fandom.com/wiki/Judy"
+  },
+  {
+    id: 8,
+    name: "Sherb",
+    species: "Goat",
+    personality: "Lazy",
+    catchphrase: "bawwww",
+    favoriteColor: "Blue",
+    birthday: "January 18",
+    image: "https://dodo.ac/np/images/8/87/Sherb_NH.png",
+    wiki: "https://animalcrossing.fandom.com/wiki/Sherb"
+  },
+  {
+    id: 9,
+    name: "Audie",
+    species: "Wolf",
+    personality: "Peppy",
+    catchphrase: "foxtrot",
+    favoriteColor: "Orange",
+    birthday: "August 31",
+    image: "https://dodo.ac/np/images/3/30/Audie_NH.png",
+    wiki: "https://animalcrossing.fandom.com/wiki/Audie"
+  },
+  {
+    id: 10,
+    name: "Flurry",
+    species: "Hamster",
+    personality: "Normal",
+    catchphrase: "powderpuff",
+    favoriteColor: "Pink",
+    birthday: "January 30",
+    image: "https://dodo.ac/np/images/6/6c/Flurry_NH.png",
+    wiki: "https://animalcrossing.fandom.com/wiki/Flurry"
+  }
+];
+
+export default villagers;
